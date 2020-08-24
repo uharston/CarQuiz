47fcc7d6ea3279d8b036f610f605cfec1a6c7a25
uharston <uharston@gmail.com>
2020-08-24 16:11:21 -0500

    fetched makes

--- back-end/app/controllers/makes_controller.rb
+++ back-end/app/controllers/makes_controller.rb
@@ -7,7 +7,7 @@ class MakesController < ApplicationController
 
     def show 
         make = Make.find_by(make_name: params[:id])
-        render json: make.to_json(include: {models: {only: :modelname }}, only: [:make_name, :make_id])
+        render json: make.to_json(include: {models: {only: :modelname }}, only: [:make_name])
                 # render json: categories.to_json(include: {items: {only: [:name, :description, :price]}}, only: :name)
 
     end 
--- front-end/index.js
+++ front-end/index.js
@@ -1,3 +1,23 @@
 console.log("HELLO URIAH")
+const yearForm = document.getElementById("year-input")
+const makesForm = document.getElementById("makes-input")
+yearForm.addEventListener('click', sayClick)
+makesForm.addEventListener('click', fetchCarMakes)
+function sayClick() {
+    console.log("clicked yearForm!")
+}
+function fetchCarMakes() {
+    
+    fetch("http://127.0.0.1:3000/makes")
+    .then(res => res.json())
+    .then(addMakes)
+}
 
-fetch(https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json)
+function addMakes(e) {
+    
+    e.forEach( make => {
+        let newMake = document.createElement('option');
+        newMake.innerText = make.make_name 
+        makesForm.firstElementChild.appendChild(newMake)
+    })
+}
