d0a073a893c4d46f79816f92014a47e19345cd46
uharston <uharston@gmail.com>
2020-10-23 12:57:55 -0500

    added cors

--- back-end/config/initializers/cors.rb
+++ back-end/config/initializers/cors.rb
@@ -5,15 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
 
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#    end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+   end
+end
 
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #     allow do
