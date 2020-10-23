81ccc6495b63cf94fb0d6568210b4241c2a6f8d7
uharston <uharston@gmail.com>
2020-10-23 12:35:10 -0500

    migration

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_29_185616) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
